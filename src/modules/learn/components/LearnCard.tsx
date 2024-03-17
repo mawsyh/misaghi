938e941281abb412a8c22f73ef82db3e93d2072f
mawsyh <moh.mir36@gmail.com>
2024-03-17 15:55:45 +0330

    fix: better functionality of component

--- src/modules/learn/components/LearnCard.tsx
+++ src/modules/learn/components/LearnCard.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link';
 import { AiFillFire as NewIcon } from 'react-icons/ai';
 import { BiLabel as LevelIcon } from 'react-icons/bi';
 import { HiOutlineArrowSmRight as ViewIcon } from 'react-icons/hi';
-import { MdLibraryBooks as LessonIcon } from 'react-icons/md';
-import useSWR from 'swr';
 
 import Card from '@/common/components/elements/Card';
 import Image from '@/common/components/elements/Image';
 import { ContentProps } from '@/common/types/learn';
 import { fetcher } from '@/services/fetcher';
+import Skeleton from 'react-loading-skeleton';
+import SkeletonLoader from '@/common/components/elements/SkeletonLoader';
+import { MdDateRange } from 'react-icons/md';
+import { Suspense } from 'react';
 
 const LearnCard = ({
   title,
@@ -18,13 +20,14 @@ const LearnCard = ({
   is_new,
   level,
   is_medium,
+  date,
 }: ContentProps) => {
-  const { data } = useSWR(`/api/learn?slug=${slug}`, fetcher);
-
-  const lessonCount = data?.count ?? '0';
-
   return (
-    <Link href={`/learn/${slug}`}>
+    <a
+      href={`https://medium.com/@moh.mir36/${slug}`}
+      target='_blank'
+      rel='noopener noreferrer'
+    >
       <Card className='group relative border border-neutral-200 dark:border-neutral-900 lg:hover:scale-[102%] cursor-pointer'>
         {is_new && (
           <div className='flex items-center gap-1 absolute top-0 right-0 bg-yellow-300 text-emerald-950 text-[13px] font-medium py-1 px-2 rounded-bl-xl rounded-tr-xl z-[2]'>
@@ -33,14 +36,28 @@ const LearnCard = ({
           </div>
         )}
         <div className='relative'>
-          <Image
-            src={image}
-            width={400}
-            height={200}
-            alt={title}
-            className='rounded-t-xl h-48 object-cover object-left'
-          />
-          <div className='flex gap-1 absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 flex justify-center items-center text-white group-hover:opacity-80 rounded-t-xl text-sm font-medium'>
+          <Suspense
+            fallback={
+              <SkeletonLoader>
+                <Skeleton
+                  height={200}
+                  containerClassName='flex'
+                  className='rounded-t-xl h-48 object-cover object-left'
+                />
+              </SkeletonLoader>
+            }
+          >
+            {image ? (
+              <Image
+                src={image}
+                width={400}
+                height={200}
+                alt={title}
+                className='rounded-t-xl h-48 object-cover object-left w-full'
+              />
+            ) : null}
+          </Suspense>
+          <div className='flex gap-1 absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 justify-center items-center text-white group-hover:opacity-80 rounded-t-xl text-sm font-medium'>
             <span>{is_medium ? 'Read more on medium' : 'View Lessons'}</span>
             <ViewIcon size={20} />
           </div>
@@ -48,7 +65,7 @@ const LearnCard = ({
         <div className='flex flex-col justify-between p-5 space-y-3'>
           <div className='space-y-2'>
             <div className='flex justify-between'>
-              <div className='text-lg font-sora cursor-pointer text-neutral-700 dark:text-neutral-300 lg:group-hover:text-teal-600 dark:group-hover:text-teal-400 transition-all duration-300'>
+              <div className='text-lg font-sora cursor-pointer text-neutral-700 dark:text-neutral-300 lg:group-hover:text-teal-600 dark:group-hover:text-teal-400 transition-all duration-300 truncate'>
                 {title}
               </div>
             </div>
@@ -56,12 +73,10 @@ const LearnCard = ({
               {description}
             </p>
           </div>
-          <div className='flex gap-4 text-neutral-600 dark:text-neutral-400'>
+          <div className='flex justify-between gap-4 text-neutral-600 dark:text-neutral-400'>
             <div className='flex gap-1 items-center'>
-              <LessonIcon size={16} />
-              <span className='text-sm ml-0.5'>
-                {`${lessonCount} ${lessonCount > 1 ? 'Lessons' : 'Lesson'}`}
-              </span>
+              <MdDateRange size={16} />
+              <span className='text-sm ml-0.5'>{date}</span>
             </div>
             <div className='flex gap-1 items-center'>
               <LevelIcon size={16} />
@@ -70,7 +85,7 @@ const LearnCard = ({
           </div>
         </div>
       </Card>
-    </Link>
+    </a>
   );
 };
 
